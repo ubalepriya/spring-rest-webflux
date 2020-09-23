8d4a4f3cbd4957ed14643e713e7d7cec0221f62b
ubalepriya <ubale.priya@gmail.com>
2020-09-23 22:29:07 +0530

    Initial commit

---
+++ src/main/java/guru/springframework/springrestwebflux/SpringRestWebfluxApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springrestwebflux/SpringRestWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
